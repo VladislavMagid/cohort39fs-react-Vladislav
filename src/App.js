d97c31441ae0f8eae49cd0198e56dbed6b2fb5bc
VladislavMagid <Vladislav.magid@gmail.com>
2024-06-26 22:18:41 +0200

    lesson02

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import "./App.css";
+
+//Lessons
+import Lesson02 from "./lessons/Lesson02/Lesson02";
+
+//Homeworks
 
 function App() {
   return (
     <div className="App">
-        <p>
-          This is our first React app
-        </p>
+      <p>This is our first React app</p>
+      <Lesson02 />
     </div>
   );
 }
